e1b5e234e43cd9dc7bbf361b572434bf09c1c878
Light34000 <92083598+Light34000@users.noreply.github.com>
2024-02-01 11:55:20 +0100

    Update reinforcement_learning.py

--- reinforcement_learning.py
+++ reinforcement_learning.py
@@ -1,1 +1,34 @@
+import numpy as np
+
+class machine_learning :    
+
+    def __init__(self, taxi, matrice) :
+        self.taxi = taxi
+        self.matrice = matrice
+        self.indice = [0,0]
+
+    def deplacement(self, matrice) :
+        self.matrice 
+
+    def affiche(self) :
+        print(self.matrice)
+
+    def taxi_init(self) :
+        self.matrice[0,0] = self.taxi
+
+    def trouve_indice(self):
+        self.indice = list(zip(np.where(self.matrice == 1)))
+        print(self.indice)
+
+    #def droite(self):
+        
+
+#--------------------------------#
+
+M = np.zeros((3,3))
+
+test=machine_learning(1, M)
+test.taxi_init()
+test.affiche()
+test.trouve_indice()
 
