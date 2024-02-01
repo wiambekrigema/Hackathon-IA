008bdfc697d0131d43e62b30b89c81f7e5fd2558
Gamaremy <148548581+Gamaremy@users.noreply.github.com>
2024-02-01 12:20:54 +0100

    Update reinforcement_learning.py
    Initialisation

--- reinforcement_learning.py
+++ reinforcement_learning.py
@@ -1,1 +1,20 @@
+from matrice import  Matrice
 
+
+matrice = Matrice()
+matrice.placer_point(0, 4, 'G')  
+matrice.placer_point(0, 0, "R")
+matrice.placer_point(4, 0, 'Y')  
+matrice.placer_point(4, 3, "B")
+
+
+matrice.placer_point(0, 4, 'taxi')  
+matrice.afficher()
+print("Tentative de déplacement à droite:")
+matrice.deplacer((0, 1), 'droite')
+
+
+
+
+class Apprentisage:
+    print('')
