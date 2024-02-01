b22889d46bfc5bc83d8d73e88795074ecc381c3a
wiambekrigema <wiambekrigema@github.com>
2024-02-01 12:32:37 +0100

    interface graphique
    Avec Lukas, on a rajouté le tkinter pour l'aspect visuelle, il est encore wip ( work in progress )

--- taxi.py
+++ taxi.py
@@ -1,4 +1,17 @@
+import tkinter as tk
+from tkinter import messagebox
+
 class Taxi:
-  
-  def __init__(self):
-  
+    def __init__(self, fenetre):
+        self.fenetre = fenetre
+        fenetre.title("Taxi Autonome")
+
+        self.label = tk.Label(fenetre, text="Binevenue dans notre appli!")
+        self.label.pack()
+    def move_taxi(self, x, y):
+        print(f" taxi bouge ({x}, {y})")
+
+root = tk.Tk()
+app = Taxi(root)
+root.mainloop()
+
