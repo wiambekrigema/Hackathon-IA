b063eca5306d9ac1a26b3dea6ccb392f75ea4efe
wiambekrigema <wiambekrigema@github.com>
2024-02-01 13:44:30 +0100

    update taxi
    j'ai rajouter le visuelle de la grille

--- taxi.py
+++ taxi.py
@@ -1,17 +1,26 @@
 import tkinter as tk
-from tkinter import messagebox
-
-class Taxi:
-    def __init__(self, fenetre):
+from matrice import Matrice
+class TaxiApp:
+    def __init__(self, fenetre, matrice):
         self.fenetre = fenetre
-        fenetre.title("Taxi Autonome")
+        self.matrice = matrice
+        fenetre.title("Taxi Application")
 
-        self.label = tk.Label(fenetre, text="Binevenue dans notre appli!")
-        self.label.pack()
+        self.buttons = [[None for _ in range(5)] for _ in range(5)]
+        for i in range(5):
+            for j in range(5):
+                self.buttons[i][j] = tk.Button(fenetre, text=self.matrice.grille[i][j], command=lambda i=i, j=j: self.move_taxi(i, j))
+                self.buttons[i][j].grid(row=i, column=j)
+
     def move_taxi(self, x, y):
-        print(f" taxi bouge ({x}, {y})")
+        if self.matrice.est_mouvement_autorise((x, y), 'droite'):
+            print(f"Mouvement de {x}, {y} à droite autorisé.")
+            self.matrice.deplacer((x, y), 'droite')
+            for i in range(5):
+                for j in range(5):
+                    self.buttons[i][j]['text'] = self.matrice.grille[i][j]
 
 root = tk.Tk()
-app = Taxi(root)
+matrice = Matrice()
+app = TaxiApp(root, matrice)
 root.mainloop()
-
